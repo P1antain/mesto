0cbccb65e611e23080feb6474255cc34354ef613
P1antain <lifeg1ance@yandex.ru>
2021-04-01 23:10:34 +0300

    Рефакторинг popup? Работает попап изображений

--- utils/className.js
+++
@@ -1,9 +0,0 @@
-export const className = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__save',
-    inactiveButtonClass: 'popup__save_disable',
-    inputErrorClass: '.popup__input-error',
-    errorClass: 'popup__input-error_active',
-    popupSection: '.popup__section'
-};
